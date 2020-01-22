266d75fba1cb176a588cad22bec8ae377041ab30
Name <buncharo1@gmail.com>
2020-01-22 12:37:45 +0900

    初期のUnityプロジェクト
    各メンバーのフォルダーの用意とテストSceneの用意

---
+++ Assets/Kurono/Script/PublicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace game
+{
+    public class PublicManager : MonoBehaviour
+    {
+        GameObject m_DontDestroySoundOBJ;
+        GameObject m_SceneManger;
+        GameObject m_GameManager;
+        GameObject m_SoundManger;
+        GameObject m_EnemyManager;
+        GameObject m_PlayerManger;
+
+
+        public void SetDontDestroySoundOBJ(GameObject obj)
+        {
+            Destroy(m_DontDestroySoundOBJ);
+            m_DontDestroySoundOBJ = obj;
+            DontDestroyOnLoad(m_DontDestroySoundOBJ);
+        }
+
+        void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        
+    }
+
+
+}
---
+++ Assets/Kurono/Script/SingletonTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace game
+{
+    class SingletonTest : Singleton<SingletonTest>
+    {
+        float SDSA = 20;
+        public void Test()
+        {
+            Debug.Log("シングルトン！");
+        }
+        public void sdsa()
+        {
+            Debug.Log("sdsa"+SDSA);
+        }
+        public void Setsdsa(float s)
+        {
+            SDSA = s;
+            Debug.Log("Set:"+SDSA);
+        }
+    }
+    
+}
